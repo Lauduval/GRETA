0c5762314c41714c7ff86136b30e91a0e6b24e94
Lauduval <lau.duval@gmail.com>
2020-01-13 16:55:26 +0100

    inclusion de la page php 'station'

---
+++ work1/station.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+    
+    <head>
+        <title>les station</title>
+        <meta charset="utf-8"/>
+        <link rel="stylesheet" type="text/css" href="stylesheet.css">
+    </head>
+    
+    <body>
+      
+      <?php
+       
+       /***********************************************************
+       Nom du script : staion.php
+       Description   : script de connection a la base de donnée "Agence de voyage"
+       interrogation et description de la table station, affichage du résultat dans un tableau html
+       Auteur        : L.HIDEUX
+       Version       : 1.0
+       Date          : 13/01/2020
+       ************************************************************/
+        
+        ?>
+        
+        <h1>les Stations</h1>
+        
+    </body>
+</html>
